b8402142efdab22cdebfdd566618eb06d543364c
Derek Nie <dnie63@berkeley.edu>
2020-04-27 00:00:17 -0400

    class weights*2, one dense layer, hidden size 60, batch size 20

--- models/bilstm_eval.py
+++ models/bilstm_eval.py
@@ -42,15 +42,15 @@ def create_bilstm():
     input_title = keras.layers.Input(shape = (num_title_embeddings, embedding_size))
 
     # BiLSTM layer that reads in a title input
-    flayer_title = LSTM(40, return_state=True)
-    blayer_title = LSTM(40, return_state=True, go_backwards=True)
+    flayer_title = LSTM(60, return_state=True)
+    blayer_title = LSTM(60, return_state=True, go_backwards=True)
     lstm_title, fh_title, fc_title, bh_title, bc_title = Bidirectional(flayer_title, backward_layer=blayer_title)(input_title)
 
     input_body = keras.layers.Input(shape = (num_body_embeddings, embedding_size))
 
     # BiLSTM layer that reads in a body input and uses the previous layer's output as initial states
-    flayer_body = LSTM(40, return_state=True)
-    blayer_body = LSTM(40, return_state=True, go_backwards=True)
+    flayer_body = LSTM(60, return_state=True)
+    blayer_body = LSTM(60, return_state=True, go_backwards=True)
     lstm_body, fh_body, fc_body, bh_body, bc_body = Bidirectional(flayer_body, backward_layer=blayer_body)\
         (input_body, initial_state=[fh_title, fc_title, bh_title, bc_title])
 
