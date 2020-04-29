832487898b52b77e5b755e18ae8d6e882425e92b
Carly Feng <czf@berkeley.edu>
2020-04-28 20:39:03 -0700

    fixed fake & real

--- extension/src/display.js
+++ extension/src/display.js
@@ -8,8 +8,8 @@ try {
 // history.replace({}, document.title, location.origin + location.pathname);
 
 if (sharedData.p_class == 0) {
-    document.getElementById('predicted class').textContent += "real";
-} else {
     document.getElementById('predicted class').textContent += "fake";
+} else {
+    document.getElementById('predicted class').textContent += "real";
 }
 document.getElementById('score').textContent += sharedData.p_score;
