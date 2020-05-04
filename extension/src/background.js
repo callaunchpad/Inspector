333fe02c30776c8d52f703abfde900b9a7cbb1b0
David Shau <davidshau22@berkeley.edu>
2020-05-04 00:25:01 -0700

    added Inspector icon, organized icons into folder, possibly fixed the intermittent alert issue?

--- extension/src/background.js
+++ extension/src/background.js
@@ -5,36 +5,53 @@ console.log("backgroud running");
 import Mercury from '@postlight/mercury-parser';
 import inference from './posts.js'
 
+// load the json of sources first
+async function loadSources() {
+    let res = await fetch("./sources.json");
+    let json = await res.json();
+    return json;
+}
+// global sources dictionary so don't have to keep loading it every time
+var sources = null;
 
 chrome.browserAction.onClicked.addListener(async (tab) => {
+    // load json sources array globally if not null
+    if (sources === null) {
+        sources = await loadSources();
+    }
+    console.log(sources);
+
     // remove "default_popup": "popup.html" from manifest for this to work
-    console.log("sending request...")
     // tab param has info about the tab you're on
 
     chrome.tabs.query({active: true, lastFocusedWindow: true}, async (tabs) => {
         // send a message to trigger an alert via content.js
-        chrome.tabs.sendMessage(tab.id);
+        chrome.tabs.sendMessage(tabs[0].id, null);
 
         let url = tabs[0].url;
         console.log("url: ", url);
 
         // get webpage information with the Mercury package
-        let [webpageInfo, sourceType] = await Promise.all([Mercury.parse(url), checkSources(url)]);
-        if (sourceType != null) {
+        let webpage_info = await Mercury.parse(url);
+        
+        let source_type = checkSources(url, sources);
+        console.log("type: ", source_type);
+        if (source_type != null) {
             // found the webpage is list of sources
-            if (sourceType === "trusted") {
+            if (source_type === "trusted") {
                 console.log("this is a trusted source!");
             } else {
-                console.log("this is sketchy source: ", sourceType);
+                console.log("this is sketchy source: ", source_type);
             }
             return;
         }
         // remove html tags with regex
-        let articleBody = webpageInfo.content.replace( /(<([^>]+)>)/ig, '');
-        let articleTitle = webpageInfo.title;
-        console.log("title: ", articleTitle);
-        console.log("body: ", articleBody);
+        let article_body = webpage_info.content.replace( /(<([^>]+)>)/ig, '');
+        let article_title = webpage_info.title;
+        console.log("title: ", article_title);
+        console.log("body: ", article_body);
 
+        console.log("sending request...")
         let model_result = await inference(article_title, article_body);
 
         let { cnn_probability, cnn_class, 
@@ -43,7 +60,6 @@ chrome.browserAction.onClicked.addListener(async (tab) => {
             article3_class, article3_link, 
             article4_class, article4_link } = model_result;
 
-        console.log("article1 class is: " + article1_class + " and article 2 class is: " + article2_class);
         chrome.windows.create({url: "display.html?data=" + encodeURIComponent(JSON.stringify({
             p_class: cnn_class, 
             p_score: cnn_probability, 
@@ -62,10 +78,8 @@ chrome.browserAction.onClicked.addListener(async (tab) => {
 // checks to see if the webpage url is on our list of 
 // trusted/sketch sources. Returns source type as a string if it is,
 // otherwise it returns null.
-async function checkSources(url) {
+function checkSources(url, sources) {
     let hostName = getHostName(url);
-    let res = await fetch("./sources.json");
-    let sources = res.json();
     if (sources[hostName] != null) {
         return sources[hostName].type;
     }
@@ -75,6 +89,7 @@ async function checkSources(url) {
 // extracts just the hostname of a url using regex
 function getHostName(url) {
     let hostname = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+    console.log("hostname: ", hostname);
     if (hostname != null && hostname.length > 2 && typeof hostname[2] === 'string' && hostname[2].length > 0) {
         console.log("hostname: ", hostname[2]);
         return hostname[2];
